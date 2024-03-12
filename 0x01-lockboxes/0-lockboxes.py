9fca125b7dcbad3a21316709448de3c3282942ea
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-12 13:19:31 -0700

    Solve the problem using Bread-First Search algorithm

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,10 +6,10 @@ can be opened using the provided keys in a given configuration.
 """
 
 
+# Using Depth-First Search (DFS) algorithm.
 def canUnlockAll(boxes):
     """
     This function checks if all boxes can be opened using the provided keys
-    using Depth-First Search (DFS) algorithm.
 
     Args:
         boxes: A list of lists, where each inner list represents
@@ -36,3 +36,41 @@ def canUnlockAll(boxes):
                 keys_to_explore.append(key)
 
     return len(visited_boxes) == boxes_num
+
+
+# Using Breadth-First Search (BFS) algorithm.
+
+# from collections import deque
+
+# def canUnlockAll(boxes):
+#     """
+#     This function checks if all boxes can be opened using the provided keys
+#     using Breadth-First Search (BFS) algorithm.
+
+#     Args:
+#         boxes: A list of lists, where each inner list represents
+#         the keys found in a box.
+
+#     Returns: True if all boxes can be opened, False otherwise.
+#     """
+#     # Track visited boxes
+#     visited_boxes = [0]
+
+#     # Track keys to explore (FIFO - stack behavior)
+#     keys_to_explore = deque()
+#     keys_to_explore.append(0)
+
+#     boxes_num = len(boxes)
+
+#     while keys_to_explore:
+#         # Get the next key to explore
+#         current_key = keys_to_explore.popleft()
+#         print(f'Exploring key for box number: {current_key}')
+
+#         # Check keys available in the box associated with the current key
+#         for key in boxes[current_key]:
+#             if key not in visited_boxes and key < boxes_num:
+#                 visited_boxes.append(key)
+#                 keys_to_explore.append(key)
+
+#     return len(visited_boxes) == boxes_num
