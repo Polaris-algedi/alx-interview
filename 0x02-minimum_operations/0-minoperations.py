780e87d430fc41c3612fda56cc0c62867894b578
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-20 14:02:17 -0700

    Fix the code style

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,20 +3,29 @@
 Greedy Algorithms: Minimum Operations to Obtain 'H' Characters
 """
 
+
 def minOperations(n):
     """
-    Calculates the minimum number of operations needed to obtain n 'H' characters.
+    Calculates the minimum number of operations needed to
+    obtain n 'H' characters.
 
     Args:
         n (int): The target number of 'H' characters.
 
     Returns:
-        int: The minimum number of operations required. If n is less than or equal to 1, returns 0.
+        int: The minimum number of operations required.
+        If n is less than or equal to 1, returns 0.
 
     Notes:
-        In order to solve this problem efficiently, you should understand the following concepts:
-        1. Prime factorization: Breaking down a number into its prime factors. Adding the prime factors of a number will magically be the number of operations needed to obtain that number of 'H' characters.
-        2. "If b is a multiple of a, then any number divisible by b is also divisible by a.": Knowing this rule simplifies the solution process because only prime factors will be added as operations, and non-prime numbers will not contribute to the operation count.
+        In order to solve this problem efficiently, you should understand
+        the following concepts:
+        1. Prime factorization: Breaking down a number into its prime factors.
+        Adding the prime factors of a number will magically be the number of
+        operations needed to obtain that number of 'H' characters.
+        2. "If b is a multiple of a, then any number divisible by b is also
+        divisible by a.": Knowing this rule simplifies the solution process
+        because only prime factors will be added as operations, and non-prime
+        numbers will not contribute to the operation count.
 
     Example:
         >>> minOperations(9)
@@ -28,14 +37,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-        
+
     return operations
