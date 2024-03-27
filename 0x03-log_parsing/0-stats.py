db7d471bcb7be1badd0b90757b2eab86bd8119a8
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-27 07:27:34 +0000

    Remove log_parsing function

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -33,45 +33,35 @@ def print_statistics(status_dict, file_size):
             print('{}: {}'.format(code, status_dict[code]))
 
 
-def log_parsing():
-    """
-    Parse the log file and print statistics.
+status_dict = {
+    200: 0, 301: 0,
+    400: 0, 401: 0,
+    403: 0, 404: 0,
+    405: 0, 500: 0
+}
+total_size = 0
+count = 1
+try:
+    for line in sys.stdin:
+        if line.count(' ') != 8:
+            continue
+        try:
+            # Get the file size from a log line.
+            file_size = re.search(r'\d+$', line)
+            total_size += int(file_size.group())
 
-    Returns:
-        None
-    """
-    status_dict = {
-        200: 0, 301: 0,
-        400: 0, 401: 0,
-        403: 0, 404: 0,
-        405: 0, 500: 0
-    }
-    total_size = 0
-    count = 1
-    try:
-        for line in sys.stdin:
-            if line.count(' ') != 8:
-                continue
-            try:
-                # Get the file size from a log line.
-                file_size = re.search(r'\d+$', line)
-                total_size += int(file_size.group())
+            # Get the status code from a log line.
+            status_code_match = re.search(
+                r' 200|301|400|401|403|404|405|500 ', line)
+            status_code = int(status_code_match.group())
+            status_dict[status_code] += 1
+        except (TypeError, AttributeError, ValueError):
+            continue
 
-                # Get the status code from a log line.
-                status_code_match = re.search(
-                    r' 200|301|400|401|403|404|405|500 ', line)
-                status_code = int(status_code_match.group())
-                status_dict[status_code] += 1
-            except (TypeError, AttributeError, ValueError):
-                continue
-
-            if count == 10:
-                print_statistics(status_dict, total_size)
-                count = 1
-            else:
-                count += 1
-    except KeyboardInterrupt:
-        print_statistics(status_dict, total_size)
-
-
-log_parsing()
+        if count == 10:
+            print_statistics(status_dict, total_size)
+            count = 1
+        else:
+            count += 1
+except KeyboardInterrupt:
+    print_statistics(status_dict, total_size)
