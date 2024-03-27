50841251a6f275c95fcd02d150b5702a00bb0a61
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-27 08:07:20 +0000

    Fix issue with parsing log file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -36,14 +36,17 @@ try:
             counter += 1
 
             if counter <= 10:
-                # Extract file size and status code
-                file_size = int(parsed_line[-1])
-                # Second last element is status code
-                status_code = parsed_line[-2]
+                try:
+                    # Extract file size and status code
+                    file_size = int(parsed_line[-1])
+                    # Second last element is status code
+                    status_code = parsed_line[-2]
 
-                if status_code in status_dict:
-                    total_file_size += file_size
-                    status_dict[status_code] += 1
+                    if status_code in status_dict:
+                        total_file_size += file_size
+                        status_dict[status_code] += 1
+                except (ValueError, IndexError):
+                    continue
 
             if counter == 10:
                 print_statistics(status_dict, total_file_size)
