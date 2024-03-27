835fd6eafb84a90a9cddf29ccef8d383880edcec
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-27 07:57:54 +0000

    Refactor the code. Use split instead of regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""
-This module provides functions to parse log files and print statistics.
-
-The module contains the following functions:
-- print_statistics(status_dict, file_size): Prints the statistics
-of the log file.
-"""
 
 import sys
-import re
 
 
-def print_statistics(status_dict, file_size):
+def print_statistics(status_dict, total_file_size):
     """
     Print the statistics of the log file.
 
@@ -23,34 +15,39 @@ def print_statistics(status_dict, file_size):
     Returns:
         None
     """
-    print('File size:', file_size)
+    print("File size:", total_file_size)
     for code, count in sorted(status_dict.items()):
-        if count:
-            print(f"{code}: {count}")
+        if count != 0:
+            print("{}: {}".format(code, count))
 
 
-status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-total_size = 0
-count = 0
+total_file_size = 0
+counter = 0
+status_dict = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0
+}
 
 try:
     for line in sys.stdin:
-        # Extract file size and status code
-        file_size_match = re.search(r'\b(\d+)\s*$', line)
-        status_code_match = re.search(
-            r'\b(200|301|400|401|403|404|405|500)\b', line)
+        parsed_line = line.strip().split()
 
-        if file_size_match and status_code_match:
-            file_size = int(file_size_match.group(1))
-            total_size += file_size
+        if len(parsed_line) >= 3:
+            counter += 1
 
-            status_code = int(status_code_match.group(1))
-            status_dict[status_code] += 1
+            if counter <= 10:
+                # Extract file size and status code
+                file_size = int(parsed_line[-1])
+                # Second last element is status code
+                status_code = parsed_line[-2]
 
-            count += 1
-            if count == 10:
-                print_statistics(status_dict, total_size)
-                count = 0
+                if status_code in status_dict:
+                    total_file_size += file_size
+                    status_dict[status_code] += 1
 
-except KeyboardInterrupt:
-    print_statistics(status_dict, total_size)
+            if counter == 10:
+                print_statistics(status_dict, total_file_size)
+                counter = 0
+
+finally:
+    print_statistics(status_dict, total_file_size)
