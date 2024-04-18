fe73996d909a67ea9ea8d3d5d4d498b3e16594ce
Polaris-algedi <karchalmohammed@gamil.com>
2024-04-18 19:30:14 +0100

    Refactor Star Wars API code to use request module instead of fetch

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,24 @@
 #!/usr/bin/node
 
-const request = require('request');
-const starWarsAPI = 'https://swapi-api.alx-tools.com/api/films/';
-const movieID = process.argv[2].toString();
+const req = require('request');
 
-// Function to fetch film data
-function fetchFilmData (movieID) {
-  return new Promise((resolve, reject) => {
-    const filmUrl = `${starWarsAPI}${movieID}`;
-    request(filmUrl, (error, _, body) => {
-      if (error) reject(error);
-      else resolve(JSON.parse(body));
-    });
-  });
-}
-
-// Function to fetch character data and print names recursively
-function fetchAndPrintCharacters (casts, index = 0) {
-  if (index < casts.length) {
-    request(casts[index], (error, _, body) => {
-      if (error) console.error(error);
-      else {
-        const character = JSON.parse(body);
-        console.log(character.name);
-        fetchAndPrintCharacters(casts, index + 1);
-      }
-    });
+req(
+  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  async (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      const chars = JSON.parse(body).characters;
+      chars.forEach(async (char) => {
+        const promise = new Promise((resolve, reject) => {
+          req(char, function (err, res, body) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(JSON.parse(body).name);
+            }
+          });
+        });
+        console.log(await promise);
+      });
+    }
   }
-}
-
-// Main function to fetch film data and print characters
-async function main () {
-  try {
-    const filmData = await fetchFilmData(movieID);
-    const casts = filmData.characters;
-    fetchAndPrintCharacters(casts);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-main();
+);
