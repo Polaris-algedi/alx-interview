1c2aba655fe145f97d5f4b64bdbd23ecd99d3c4c
Polaris-algedi <karchalmohammed@gamil.com>
2024-04-18 18:22:08 +0100

    Refactor Star Wars API code to use request module instead of fetch

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,42 @@
 #!/usr/bin/node
 
+const request = require("request");
+
 /**
  * Retrieves Star Wars character names from a given movie.
  * @param {number} movieId - The ID of the Star Wars movie.
  * @returns {Promise<void>} - A promise that resolves with the character names.
  */
-async function getStarWarFilm(movieId) {
-  const res = await fetch(
-    `https://swapi-api.alx-tools.com/api/films/${movieId}`
-  );
-  const json = await res.json();
+function getStarWarFilm(movieId) {
+  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-  const charactersNamesPromises = json.characters.map((url) =>
-    getCharacterName(url)
-  );
-  const charactersNames = await Promise.all(charactersNamesPromises);
-  charactersNames.forEach((name) => {
-    console.log(name);
+  return new Promise((resolve, reject) => {
+    request(filmUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        reject(
+          `Failed to fetch film data. Status code: ${response.statusCode}`
+        );
+        return;
+      }
+
+      const filmData = JSON.parse(body);
+      const characterURLs = filmData.characters;
+
+      const characterNamesPromises = characterURLs.map((url) =>
+        getCharacterName(url)
+      );
+      Promise.all(characterNamesPromises)
+        .then((characterNames) => {
+          characterNames.forEach((name) => console.log(name));
+          resolve();
+        })
+        .catch((error) => reject(error));
+    });
   });
 }
 
@@ -25,12 +45,26 @@ async function getStarWarFilm(movieId) {
  * @param {string} url - The URL of the Star Wars character.
  * @returns {Promise<string>} - A promise that resolves with the character name.
  */
-async function getCharacterName(url) {
-  const res = await fetch(url);
-  const character = await res.json();
+function getCharacterName(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+        return;
+      }
 
-  return character.name;
+      if (response.statusCode !== 200) {
+        reject(
+          `Failed to fetch character data. Status code: ${response.statusCode}`
+        );
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      resolve(characterData.name);
+    });
+  });
 }
 
 const movieId = process.argv[2];
-getStarWarFilm(movieId);
+getStarWarFilm(movieId).catch((error) => console.error("Error:", error));
