366824a11edc1c91d03b8d22367f2a94983493c4
Polaris-algedi <karchalmohammed@gamil.com>
2024-04-18 18:51:21 +0100

    Refactor Star Wars API code to use request module instead of fetch

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,70 +1,43 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
+const starWarsAPI = 'https://swapi-api.alx-tools.com/api/films/';
+const movieID = process.argv[2].toString();
 
-/**
- * Retrieves Star Wars character names from a given movie.
- * @param {number} movieId - The ID of the Star Wars movie.
- * @returns {Promise<void>} - A promise that resolves with the character names.
- */
-function getStarWarFilm(movieId) {
-  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
+// Function to fetch film data
+function fetchFilmData (movieID) {
   return new Promise((resolve, reject) => {
-    request(filmUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        reject(
-          `Failed to fetch film data. Status code: ${response.statusCode}`
-        );
-        return;
-      }
-
-      const filmData = JSON.parse(body);
-      const characterURLs = filmData.characters;
-
-      const characterNamesPromises = characterURLs.map((url) =>
-        getCharacterName(url)
-      );
-      Promise.all(characterNamesPromises)
-        .then((characterNames) => {
-          characterNames.forEach((name) => console.log(name));
-          resolve();
-        })
-        .catch((error) => reject(error));
+    const filmUrl = `${starWarsAPI}${movieID}`;
+    request(filmUrl, (error, _, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
     });
   });
 }
 
-/**
- * Retrieves the name of a Star Wars character from a given URL.
- * @param {string} url - The URL of the Star Wars character.
- * @returns {Promise<string>} - A promise that resolves with the character name.
- */
-function getCharacterName(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-        return;
+// Function to fetch character data and print names recursively
+function fetchAndPrintCharacters (casts, index = 0) {
+  if (index < casts.length) {
+    request(casts[index], (error, _, body) => {
+      if (error) console.error(error);
+      else {
+        const character = JSON.parse(body);
+        console.log(character.name);
+        fetchAndPrintCharacters(casts, index + 1);
       }
-
-      if (response.statusCode !== 200) {
-        reject(
-          `Failed to fetch character data. Status code: ${response.statusCode}`
-        );
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      resolve(characterData.name);
     });
-  });
+  }
 }
 
-const movieId = process.argv[2];
-getStarWarFilm(movieId).catch((error) => console.error("Error:", error));
+// Main function to fetch film data and print characters
+async function main () {
+  try {
+    const filmData = await fetchFilmData(movieID);
+    const casts = filmData.characters;
+    fetchAndPrintCharacters(casts);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+main();
